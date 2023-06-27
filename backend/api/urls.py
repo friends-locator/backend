8fedb949f21f8abd7f86e8df7b51a5c459a0482a
Ларькин Михаил <jetpilot314@gmail.com>
2023-06-27 21:50:54 +0300

    Testing workflow and deploy.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,27 @@
+from django.db import router
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from api.views import TagViewSet
+
 from users.views import CustomUserViewSet
 
 app_name = "api"
 
-router = DefaultRouter()
-router.register("tags", TagViewSet)
 router.register("users", CustomUserViewSet)
+include(router.urls)
+path("v1/", include("djoser.urls"))
+path("v1/", include("djoser.urls.jwt")),
 
-urlpatterns = [
-    path("v1/", include(router.urls)),    path("v1/", include("djoser.urls")),    path("v1/", include("djoser.urls.jwt")),
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
 ]
