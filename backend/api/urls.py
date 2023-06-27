4019aaefbf719dc501bab687f562c28707e62cb3
Ларькин Михаил <jetpilot314@gmail.com>
2023-06-27 21:45:05 +0300

    Testing workflow and deploy.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
+
 app_name = "api"
 
-urlpatterns = [path("v1/", include("djoser.urls")),path("v1/", include("djoser.urls.jwt")),
+urlpatterns = [
+    path("v1/", include("djoser.urls")),
+    path("v1/", include("djoser.urls.jwt")),
 ]
