a1d42746bf8b85c8a75adfc43793d2d8a115ce0d
Ларькин Михаил <jetpilot314@gmail.com>
2023-07-17 20:28:14 +0300

    Testing CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,8 +65,8 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
-    # "http://127.0.0.1:8000",
-    # "null",
+    "http://127.0.0.1:8000",
+    "null",
 )
 
 
