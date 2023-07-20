6270643d0a32b18680c21a97e4c4e81368850851
Ларькин Михаил <jetpilot314@gmail.com>
2023-07-21 00:46:57 +0300

    Fixed flake8.

--- backend/places/views.py
+++ backend/places/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from .models import Place, SharedPlaces
