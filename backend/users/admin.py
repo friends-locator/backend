8c835d018a37cf7507032e47c690eb8f740eb395
Fyodor Antonevich <Levity537@gmail.com>
2023-07-21 08:20:22 +0300

    fixing isort issue

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from users.models import (CustomUser, FriendsCategory,
-                          FriendsRelationship, FriendsRequest,
-                          Status, Tag)
+from users.models import (CustomUser, FriendsCategory, FriendsRelationship,
+                          FriendsRequest, Status, Tag)
 
 
 @admin.register(Tag)
